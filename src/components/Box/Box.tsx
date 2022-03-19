e54b2929be37c7ab03b2e66b502b959ac26da81a
Arun Govindappa <govindappaarun@gmail.com>
2022-03-19 15:39:09 +0530

    update box props

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Space } from "../types/types";
 import { StyledBox } from "./Box.styled";
-import { Flex, JC, AI } from "./types";
+import { Flex, JC, AI, Wrap, AS, Direction } from "./types";
 
 export type BoxProps = {
   flex?: Flex;
   justifyContent?: JC;
   alignItems?: AI;
+  alignSelf?: AS;
+  gap?: Space;
   children?: React.ReactNode;
+  wrap?: Wrap;
+  direction?: Direction;
+  shrink?: 0 | 1;
+  grow?: 0 | 1;
+  order?: number;
 };
 
 export default function Box({ flex = "flex", children, ...rest }: BoxProps) {
