567cc06d1b141fdfd0bf27646dbe7563870ccc35
Arun Govindappa <govindappaarun@gmail.com>
2022-03-21 00:26:49 +0530

    initialize cart and wishlist page

---
+++ src/pages/cart/Cart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../header";
+import { Wrapper } from "./Cart.styled";
+
+export default function Cart() {
+  return (
+    <Wrapper>
+      <Header />
+    </Wrapper>
+  );
+}
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,40 +1,32 @@
 import React from "react";
 import Avatar from "src/components/Avatar/Avatar";
 import Badge from "src/components/Badge";
+import Box from "src/components/Box";
 import Button from "src/components/Button";
-import { CartIcon, WishIcon, UserIcon } from "src/components/Icon";
+import Checkbox from "src/components/Checkbox";
+import {
+  CartIcon,
+  WishIcon,
+  UserIcon,
+  CreditCardIcon,
+  HomeIcon,
+} from "src/components/Icon";
 import Navbar from "src/components/Navbar/Navbar";
 import Typography from "src/components/Typography/Typography";
+import { ReactComponent as Cart } from "../../assets/svg/shopping-cart.svg";
 
 export default function Test() {
   return (
     <div className="App">
-      <Button color="primary" size="lg">
-        Test
-      </Button>
-      <Button color="success" size="md" outline>
-        Test
-      </Button>
-      <Button color="warning" size="md" outline disabled>
-        Test
-      </Button>
-      <Button color="warning" size="sm">
-        Test
-      </Button>
-      <Button color="error" size="xlg" outline>
-        Test
-      </Button>
-      Hello React!, I am coming
-      <Typography variant="hero">Lorem ipsum dolor sit amet.</Typography>
-      <Typography variant="h3" color="success">
-        Lorem ipsum dolor sit amet.
-      </Typography>
-      <WishIcon size="xs" color="#F5C0B8" />
-      <WishIcon size="md" color="#F5C0B8" />
-      <WishIcon size="lg" color="#F5C0B8" />
-      <WishIcon size="xlg" color="#F5C0B8" />
-      <CartIcon color="#C6F0C2" />
-      <UserIcon />
+      <Box display="flex" alignItems="strech" justifyContent="end">
+        <Button color="primary">Primary</Button>
+        <Button color="warning" outline>
+          Warning
+        </Button>
+      </Box>
+      <CartIcon color="red" hoverColor="green" />
+      <WishIcon />
+      <Checkbox label="Test Label" />
     </div>
   );
 }
